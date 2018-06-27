f96094502b5d65ebb2ca5ab86d2e6eca787fc728
ankushkarwa2705 <ankushkarwa2705@gmail.com>
2018-06-27 14:22:58 +0530

    Update DemoAppScripts.js

--- DemoAppScripts.js
+++ DemoAppScripts.js
@@ -2,12 +2,7 @@ var app1= angular.module('DemoApp', [])
     app1.controller('Controller', function ($scope,$http)
      {
         $scope.regex = '\\d+';
-        $scope.multiply = function () 
-        {
-            var product = parseInt($scope.firstnumber) * parseInt($scope.secondnumber);
-            $scope.result = product;
-        };
-        $scope.postRequest=function()
+        $scope.save_data=function()
         {
             $http.post('/saveData',$scope.formdata).then(function(response){
             alert("Submitted successfully...");
@@ -18,8 +13,7 @@ var app1= angular.module('DemoApp', [])
         $scope.getData=function(){
             $http.get('/getData').then((response)=>{
                 var text="Number_1 : "+response.data["number11"]+" Number_2 : "+response.data["number22"]+" Product : "+response.data["product12"];
-                $scope.replace= "Recent DataBase Record";
-                $scope.loadDB=text;
+                $scope.LastDBEntry=text;
             },(response)=>{
                 $scope.getErrMsg="errorstatus: "+response.status.Text;
             });
