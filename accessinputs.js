092dd00bb755a1b8de472ff535418ffcf53f34e8
ankushkarwa2705 <ankushkarwa2705@gmail.com>
2018-06-27 12:35:48 +0530

    Update accessinputs.js

--- accessinputs.js
+++ accessinputs.js
@@ -2,32 +2,53 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 var fs = require('fs');
 var express = require('express');
-const bodyParse = require('body-parser');
+var bodyParse = require('body-parser');
 var app = express();
+var num1, num2, mul_result;
+
 app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
 app.use(express.static(__dirname + "/"));
 
-app.get('/AngularApp.html', function (req, res) {
-  res.sendFile(__dirname + "/AngularApp.html");
-});
-
-app.get('/AngularApp', function (req, res) {
+app.get('/getData', (req, resp) => {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("caldb");
-    var number1 = req.query.firstnumber;
-    var number2 = req.query.secondnumber;
+    var coll = dbo.collection('multiplydb');
+    coll.count().then((count) => {
+      console.log(count);
+      coll.find({}).toArray(function (err, res) {
+        if (err) throw err;
+        num1 = res[count - 1].Number_1;
+        num2 = res[count - 1].Number_2;
+        mul_result = res[count - 1].Result;
+        inserttext = "Number_1 = " + num1 + "   Number_2 = " + num2 + "   Product : " + mul_result;
+        console.log(inserttext);
+        response = { number11: num1, number22: num2, product12: mul_result };
+        resp.send(JSON.stringify(response));
+
+        console.log(" sent response as : " + response.number11 + " " + response.number22 + " " + response.product12);
+      });
+    });
+  });
+});
+
+app.post('/saveData', function (req, result) {
+  console.log(req.body.firstnumber + " " + req.body.secondnumber);
+  result.end();
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("caldb");
+    var number1 = req.body.firstnumber;
+    var number2 = req.body.secondnumber;
     var multiply = number1 * number2;
     var myobj = { Number_1: number1, Number_2: number2, Result: multiply };
+    console.log(myobj);
     dbo.collection("multiplydb").insertOne(myobj, function (err, res) {
       if (err) throw err;
       console.log("1 document inserted");
       console.log(res.insertedCount);
-      var inserttext = "Number_1 = " + myobj.Number_1 + " Number_2 = " + myobj.Number_2 + " Product : " + myobj.Result;
-      fs.writeFile('last_db_record.txt', inserttext, function (err) {
-        if (err) throw err;
-        console.log('Replaced!');
-      });
       db.close();
     });
   });
