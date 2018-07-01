33cebe69cec967dc74b14dbb4fcfadf62e1d2435
ankushkarwa2705 <ankushkarwa2705@gmail.com>
2018-07-01 17:47:47 +0530

    Update DemoAppScripts.js

--- DemoAppScripts.js
+++ DemoAppScripts.js
@@ -1,21 +1,17 @@
 var app1= angular.module('DemoApp', [])
-    app1.controller('Controller', function ($scope,$http)
+    app1.controller('Controller', function ($scope, $http)
      {
         $scope.regex = '\\d+';
         $scope.save_data=function()
         {
-            $http.post('/saveData',$scope.formdata).then(function(response){
+            $http.post('/saveData',$scope.formdata).then(function(){
             alert("Submitted successfully...");
-            }).catch(function(response){
-                alert("error...");
             })
         }
         $scope.getData=function(){
-            $http.get('/getData').then((response)=>{
-                var text="Number_1 : "+response.data["number11"]+" Number_2 : "+response.data["number22"]+" Product : "+response.data["product12"];
+            $http.get('/getData').then(function(response){
+                var text="Number_1 : " + response.data["number11"] + " Number_2 : " + response.data["number22"] + " Product : " + response.data["product12"];
                 $scope.LastDBEntry=text;
-            },(response)=>{
-                $scope.getErrMsg="errorstatus: "+response.status.Text;
             });
         }
         $scope.getData();
